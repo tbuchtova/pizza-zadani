d7d417f63f1d9684df757b3c478c7b09b0a4c91e
Tereza Buchtová <buchtova.terez@gmail.com>
2022-10-23 12:18:46 +0200

    počet toppings a cena

--- src/components/ToppingsSelect/index.jsx
+++ src/components/ToppingsSelect/index.jsx
@@ -4,15 +4,24 @@ import './style.css';
 
 const ToppingsSelect = ({ toppings }) => {
 
-  const [checked, setChecked] = useState(toppings);
+  const [toppingState, setToppingState] = useState(toppings);
   const [count, setCount] = useState(0);
   const [price, setPrice] = useState(0);
 
-  const handleChecked = (newTopping, i) => {
-    setChecked((checked) => {
-      const selectedToppings = [...checked];
-      selectedToppings[i].selected = newTopping;
-      return selectedToppings;
+  const handleChecked = (selectVal, i) => {
+    console.log(selectVal)
+    console.log(i)
+
+    setToppingState((toppingState) => {
+      const newToppings = [...toppingState];
+      newToppings[i].selected = selectVal;
+      const selectedToppings = toppingState.filter((topping) => topping.selected === true)
+      console.log(selectedToppings)
+      setCount(selectedToppings.length)
+      
+      const priceSelected = selectedToppings.reduce((acc, topping) => topping.price + acc, 0);
+      setPrice(priceSelected)
+      return newToppings;
     })
   }
 
@@ -24,7 +33,7 @@ const ToppingsSelect = ({ toppings }) => {
       <div className="toppings">
         {toppings.map((topping, i) => (
           <Topping topping={topping} key={topping.name} 
-          onSelectedChange={(newTopping) => handleChecked(newTopping, i)}
+          onSelectedChange={(selectVal) => handleChecked(selectVal, i)}
           checked={topping.checked}
           />
         ))}
